198569e3cb89b855f1e54f643ca9a27448a2224b
nitishxthakur <158060589+nitishxthakur@users.noreply.github.com>
2025-05-02 07:45:40 +0530

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1Gzfv4ZntJr90HXuovNR5eeW7IMv4cdPh
 """
 
-!pip install serpapi sentence-transformers scikit-learn
 
 import requests
 from sentence_transformers import SentenceTransformer
